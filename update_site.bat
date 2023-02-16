4abc8573a0aef39893301a924d910cbac752b68a
Jim Ashe <ashejim@gmail.com>
2023-02-16 09:48:59 -0500

    updated resource page with individual linkable headers for videos. Fixed some style and grammar issues on resource page.

--- update_site.bat
+++ update_site.bat
@@ -1,10 +1,10 @@
 @echo off
 title compile html, save to repo folder, update GitHub repo and website
-jupyter-book build --all C:\Users\Jim\OneDrive\jupyter-books\C964
-echo "Compiling local C:\Users\Jim\OneDrive\jupyter-books\C964 ..."
-xcopy /s /e /h /i /y C:\Users\Jim\OneDrive\jupyter-books\C964 C:\Users\Jim\OneDrive\jupyter-books\github_book_repo\C964
-echo "Copied local book to ..jupyter-books\github_book_repo\C964 ..."
-cd C:\Users\Jim\OneDrive\jupyter-books\github_book_repo\C964 
+jupyter-book build --all "D:\OneDrive - Western Governors University\jupyter-books\C964"
+echo "Compiling local D:\OneDrive - Western Governors University\jupyter-books\C964 ..."
+xcopy /s /e /h /i /y "D:\OneDrive - Western Governors University\jupyter-books\C964" "D:\OneDrive - Western Governors University\jupyter-books\github_book_repo\C964"
+echo "Copied local book to D:\... \jupyter-books\github_book_repo\C964 ..."
+cd "D:\OneDrive - Western Governors University\jupyter-books\github_book_repo\C964" 
 git add ./*
 SET /P Message=Enter git commit comment: 
 git commit -m "%Message%"
@@ -15,4 +15,4 @@ ghp-import -n -p -f _build/html
 echo "Imported to git page..."
 start https://ashejim.github.io/C964/intro.html
 echo "Gitpage may take a few minutes to update. END"
-cd C:\Users\Jim\OneDrive\jupyter-books
+cd D:\OneDrive - Western Governors University\jupyter-books\C964
